cb7b77463325185946befa40d7f9054b34365f85
Lucas Araujo <lucasnsaraujo0310@gmail.com>
2023-05-07 16:04:12 -0300

    finished refresh token cycle

--- utils/withSSRAuth.ts
+++ utils/withSSRAuth.ts
@@ -1,18 +1,33 @@
 import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult } from "next";
-import { parseCookies } from "nookies";
+import { destroyCookie, parseCookies } from "nookies";
+import { AuthTokenError } from "../services/errors/AuthTokenError";
 
 export function withSSRAuth<P>(fn: GetServerSideProps<P>) {
   return async (ctx: GetServerSidePropsContext): Promise<GetServerSidePropsResult<P>> => {
     const cookies = parseCookies(ctx);
     const token = cookies['nextauth.token'];
     if (!token) {
-        return {
+      return {
         redirect: {
+          destination: '/',
+          permanent: false
+        }
+      }
+    }
+    try {
+      return await fn(ctx)
+    } catch (err) {
+      if (err instanceof AuthTokenError) {
+        destroyCookie(ctx, 'nextauth.token')
+        destroyCookie(ctx, 'nextauth.refreshToken')
+
+        return {
+          redirect: {
             destination: '/',
             permanent: false
+          }
         }
-    }   
+      }
     }
-    return await fn(ctx)
   }
 }
